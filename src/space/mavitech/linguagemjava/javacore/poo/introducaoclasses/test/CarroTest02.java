4e41979ec95c2781418ec6f044f7ba81687169f2
Marcos Vinícius Nunes <imarcosvinicius@outlook.com>
2023-11-05 13:26:46 -0300

    referencia de objetos

---
+++ src/space/mavitech/linguagemjava/javacore/poo/introducaoclasses/test/CarroTest02.java
@@ -0,0 +1,27 @@
+package space.mavitech.linguagemjava.javacore.poo.introducaoclasses.test;
+
+import space.mavitech.linguagemjava.javacore.poo.introducaoclasses.domain.Carro;
+
+public class CarroTest02 {
+
+    public static void main(String[] args) {
+
+        Carro carro01 = new Carro();
+        Carro carro02 = new Carro();
+
+        carro01.modelo = "Civic Type R";
+        carro01.marca = "Honda Motors";
+        carro01.ano = 2023;
+
+        carro02.modelo = "Tundra";
+        carro02.marca = "Toyota";
+        carro02.ano = 2022;
+
+        carro01 = carro02;
+
+        System.out.println("Modelo: " + carro01.modelo + " Marca: " + carro01.marca + " Ano: " + carro01.ano);
+        System.out.println("Modelo: " + carro02.modelo + " Marca: " + carro02.marca + " Ano: " + carro02.ano);
+
+    }
+
+}
