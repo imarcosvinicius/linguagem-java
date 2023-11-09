8d67ada0148dfc09cafb796c75743bcf10b9b8f1
Marcos Vinícius Nunes <imarcosvinicius@outlook.com>
2023-11-09 16:10:08 -0300

    modificador static

---
+++ src/space/mavitech/linguagemjava/javacore/Fmodificadorestatico/test/CarroTest01.java
@@ -0,0 +1,19 @@
+package space.mavitech.linguagemjava.javacore.Fmodificadorestatico.test;
+
+import space.mavitech.linguagemjava.javacore.Fmodificadorestatico.domain.Carro;
+
+public class CarroTest01 {
+
+    public static void main(String[] args) {
+        Carro c1 = new Carro("BMW", 280);
+        Carro c2 = new Carro("Mercedes", 275);
+        Carro c3 = new Carro("Audi", 290);
+
+        Carro.velocidadeLimite = 180;
+
+        c1.imprime();
+        c2.imprime();
+        c3.imprime();
+    }
+
+}
